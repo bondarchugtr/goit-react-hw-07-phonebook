c315214840d75f72afc41eb1d67ea015c65a33ed
Valentin Bondarchuk <bondarchukgtr@gmail.com>
2022-01-23 21:54:21 +0200

    edit store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,8 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { createLogger } from "redux-logger";
 import contactsReducer from "../redux/telbook/reducer";
-// import logger from "../helpers/logger";
 
 import {
-  // persistStore,
-  persistReducer,
   FLUSH,
   REHYDRATE,
   PAUSE,
@@ -13,31 +10,25 @@ import {
   PURGE,
   PERSIST,
 } from "redux-persist";
-import storage from "redux-persist/lib/storage";
-const persistConfig = {
-  key: "contacts",
-  storage,
-};
-// const logger = createLogger();
-// let middleware = [];
-// if (process.env.NODE_ENV === "development") {
-//   middleware = (getDefaultMiddleware) => [
-//     ...getDefaultMiddleware({
-//       serializableCheck: {
-//         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-//       },
-//     }),
-//     logger,
-//   ];
-// }
 
-const middleware = {
-  serializableCheck: {
-    ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-  },
-};
+const logger = createLogger();
+let middleware = [];
+if (process.env.NODE_ENV === "development") {
+  middleware = (getDefaultMiddleware) => [
+    ...getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+    logger,
+  ];
+}
 
-// const persisterReducer = persistReducer(persistConfig, rootReducer);
+// const middleware = {
+//   serializableCheck: {
+//     ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+//   },
+// };
 
 const rootReducer = combineReducers({
   contacts: contactsReducer,
@@ -45,8 +36,8 @@ const rootReducer = combineReducers({
 
 const store = configureStore({
   reducer: rootReducer,
-  devTools: process.env.NODE_ENV === "development",
   middleware: (getDefaultMiddleware) => getDefaultMiddleware(middleware),
+  devTools: process.env.NODE_ENV === "development",
 });
 
 // const store = configureStore({
