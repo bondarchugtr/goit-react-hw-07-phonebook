68f009bd920ebf2484279b38251e4027883a048a
Valentin Bondarchuk <bondarchukgtr@gmail.com>
2022-01-23 21:28:12 +0200

    edit store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,8 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { createLogger } from "redux-logger";
 import contactsReducer from "../redux/telbook/reducer";
+// import logger from "../helpers/logger";
+
 import {
   // persistStore,
   persistReducer,
@@ -16,29 +18,43 @@ const persistConfig = {
   key: "contacts",
   storage,
 };
-const logger = createLogger();
-let middleware = [];
-if (process.env.NODE_ENV === "development") {
-  middleware = (getDefaultMiddleware) => [
-    ...getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
-    logger,
-  ];
-}
+// const logger = createLogger();
+// let middleware = [];
+// if (process.env.NODE_ENV === "development") {
+//   middleware = (getDefaultMiddleware) => [
+//     ...getDefaultMiddleware({
+//       serializableCheck: {
+//         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+//       },
+//     }),
+//     logger,
+//   ];
+// }
+
+const middleware = {
+  serializableCheck: {
+    ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+  },
+};
+
+// const persisterReducer = persistReducer(persistConfig, rootReducer);
 
 const rootReducer = combineReducers({
   contacts: contactsReducer,
 });
-const persisterReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: persisterReducer,
-  middleware,
-  // devTools: process.env.NODE_ENV === "development",
+  reducer: rootReducer,
+  devTools: process.env.NODE_ENV === "development",
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware(middleware),
 });
+
+// const store = configureStore({
+//   reducer: persisterReducer,
+//   middleware,
+//   devTools: process.env.NODE_ENV === "development",
+// });
+
 // const persistor = persistStore(store);
 
 export default store;
