6e28ca7151fa2aadcfbb0392d4d1ad42778f56db
Valentin Bondarchuk <bondarchukgtr@gmail.com>
2022-01-23 17:19:05 +0200

    del comment code

--- src/index.js
+++ src/index.js
@@ -5,14 +5,11 @@ import App from "./App";
 import "modern-normalize";
 import store from "./redux/store";
 import { Provider } from "react-redux";
-// import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store.store}>
-      {/* <PersistGate loading={null} persistor={store.persistor}> */}
       <App />
-      {/* </PersistGate> */}
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
--- src/redux/telbook/contacts-operation.js
+++ src/redux/telbook/contacts-operation.js
@@ -1,8 +1,31 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
-
+// import // addContactRequest,
+// // addContactSuccess,
+// // addContactError,
+// // deleteContactRequest,
+// // deleteContactSuccess,
+// // deleteContactError,
+// // fetchContactsRequest,
+// // fetchContactsSuccess,
+// // fetchContactsError,
+// "./action";
 axios.defaults.baseURL =
   "https://61e892337ced4a00172ff620.mockapi.io/contacts/contacts";
+
+// export const fetchContacts = () => async (dispatch) => {
+//   dispatch(fetchContactsRequest());
+//   try {
+//     const { data } = await axios.get("/contacts");
+//     dispatch(fetchContactsSuccess(data));
+//   } catch (error) {
+//     dispatch(fetchContactsError(error));
+//   }
+//   // // axios
+//   // //   .get("/contacts")
+//   //   .then(({ data }) => dispatch(fetchContactsSuccess(data)))
+//   //   .catch((error) => dispatch(fetchContactsError(error)));
+// };
 
 export const fetchContacts = createAsyncThunk(
   "contacts/fetchContacts",
@@ -17,11 +40,27 @@ export const fetchContacts = createAsyncThunk(
   }
 );
 
+// export const addContact = (name) => (dispatch) => {
+//   const contact = {
+//     name: name.name,
+//     phone: name.number,
+//     completed: false,
+//   };
+
+//   dispatch(addContactRequest());
+
+//   axios
+//     .post("/contacts", contact)
+//     .then(({ data }) => dispatch(addContactSuccess(data)))
+//     .catch((error) => dispatch(addContactError(error)));
+// };
+
 export const addContact = createAsyncThunk(
   "contacts/addContacts",
 
   async (name) => {
     const contact = {
+      // id: nanoid(),
       name: name.name,
       phone: name.number,
     };
@@ -33,6 +72,13 @@ export const addContact = createAsyncThunk(
     }
   }
 );
+// export const deleteContact = (id) => (dispatch) => {
+//   dispatch(deleteContactRequest);
+//   axios
+//     .delete(`/contacts/${id}`)
+//     .then(() => dispatch(deleteContactSuccess(id)))
+//     .catch((error) => dispatch(deleteContactError(error)));
+// };
 
 export const deleteContact = createAsyncThunk(
   "contacts/deleteContact",
