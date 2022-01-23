ebb35c7ee667278fc577fa88319cb6952665fa32
Valentin Bondarchuk <bondarchukgtr@gmail.com>
2022-01-23 17:48:38 +0200

    add dispatch = useDispatch in app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import PhoneBook from "./Components/PhoneBook/PhoneBook";
 import s from "./Components/PhoneBook/PhoneBook.module.css";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
+import { fetchContacts } from "./redux/telbook/contacts-operation";
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (fetchContacts) {
+      dispatch(fetchContacts());
+    }
+  }, [dispatch]);
   return (
     <div className={s.Container}>
       <PhoneBook />
